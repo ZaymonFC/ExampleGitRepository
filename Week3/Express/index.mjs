6b262fd21bdc7336e70670508c1197a3611f2e86
Zaymon Foulds Cook <generation281@gmail.com>
2018-07-30 20:42:33 +1000

    Fixed node log in and ajax post

--- Week3/Express/index.mjs
+++ Week3/Express/index.mjs
@@ -5,11 +5,11 @@ const app = express()
 const port = 3000
 
 // Define the statically available content
-app.use(express.static('./public'))
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-let jsonParser = bodyParser.json()
-
-const directory = './pages'
+const directory = '/pages'
 
 app.get('/test', (req, res) => {
     let page = 'test.html'
@@ -18,13 +18,31 @@ app.get('/test', (req, res) => {
 
 app.get('/', (req, res) => {
     // res.send(`Express JS Server Running on ${port}`)
-    res.sendfile(`${directory}/index.html`)
+    res.sendFile(`${__dirname}${directory}/index.html`)
 })
 
-app.post('/login', jsonParser, (req, res) => {
+app.get('/account', (req, res) => {
+    res.sendFile(`${__dirname}${directory}/account.html`)
+})
 
-    console.log(req.body)
-    res.send('Okay')
+app.post('/login', (req, res) => {
+    const username = 'test'
+    const password = '1234'
+
+    let data = req.body
+
+    if (data[0].value === username && data[1].value === '1234') {
+        let responseData = {
+            status: true
+        }
+        res.send(responseData)
+        return
+    }
+    let responseData = {
+        status: false
+    }
+    res.send(responseData)
+    res.end()
 })
 
 app.listen(port, () => console.log(`Express app listening on port ${port}`))
